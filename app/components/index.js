9ef94aed571677d48f09a07a82e66b7cbd9bec2a
milocosmopolitan <milocosmopolitan@gmail.com>
2017-05-22 09:48:22 -0400

    indexing new components and modified home component

--- app/components/index.js
+++ app/components/index.js
@@ -1,13 +1,21 @@
 import ContentComponent from './Content';
 import DistortionComponent from './Distortion';
 import AboutComponent from './About/about';
-import BeerComponent from './Beer/beer';
 import LoginComponent from './Login/login';
-import FooterComponent from './Footer/Footer';
 
 export const Content = ContentComponent;
 export const Distortion = DistortionComponent;
-export const Beer = BeerComponent;
 export const About = AboutComponent;
 export const Login = LoginComponent;
-export const Footer = FooterComponent;
+
+// Section Components Group
+	import FooterComponent from './Footer/Footer';
+	import SplashComponent from './Splash/Splash';
+	export const Footer = FooterComponent;
+	export const Splash = SplashComponent;
+
+// Beer Components Group
+	import BeerComponent from './Beer/beer';
+	import OscillationComponent from './Beer/oscillation';
+	export const Beer = BeerComponent;
+	export const Oscillation = OscillationComponent;
