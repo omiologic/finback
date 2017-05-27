890609c1aa93dbd64a9b17a24eb810690f571cda
milocosmopolitan <milocosmopolitan@gmail.com>
2017-05-27 19:58:59 -0400

    implemented redux-persist

--- app/store.js
+++ app/store.js
@@ -1,7 +1,7 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { compose, createStore, combineReducers, applyMiddleware } from 'redux';
 import createLogger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
-
+import { persistStore, autoRehydrate } from 'redux-persist'
 import * as Reducers from './reducers/index';
 
 /* Combine Reducers */
@@ -21,4 +21,15 @@ const allReducers = combineReducers(combine(Reducers))
 /* Middleware */
 const logger = createLogger();
 
-export default createStore(allReducers, applyMiddleware(logger, thunkMiddleware));
+const store = createStore(
+	allReducers, 
+	undefined,
+	compose(
+    applyMiddleware(logger, thunkMiddleware),
+		autoRehydrate()
+  )
+);
+
+persistStore(store)
+
+export default store;
